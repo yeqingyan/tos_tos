973363af81aaa98883db4190a41f7fcc8e59b024
YEQING YAN <yyan1@mail.sfsu.edu>
2016-11-28 16:41:32 -0800

    improve format in start.s

--- source/start.s
+++ source/start.s
@@ -1,30 +1,30 @@
 .section .init   
 .global _start 
-_start:
-  ldr r0, =_vectors     // First instruction, set up the vector table 
-  mov r1, #0x0000
+_start:                 // CPU in Supervisor mode
+  ldr r0, =_vectors     // Set up the vector table, the following code copy the   
+  mov r1, #0x0000       // memory start at address _vector to 0x0000 
   ldmia r0!, {r2-r9}
   stmia r1!, {r2-r9}
-	ldmia r0!, {r2-r9}
-	stmia r1!, {r2-r9}
-	
-  cpsid i, #0x1F        // Switch back to SYS mode(0b11111)
-	
-	mov sp, #0xA00000     // Setup Stack pointer for SYS mode 
-	
-	b kernel_main         
+  ldmia r0!, {r2-r9}
+  stmia r1!, {r2-r9}
+  
+  cpsid i, #0x1F        // Switch to SYS mode(0b11111)
+  
+  mov sp, #0xA00000     // Set up Stack pointer for SYS mode 
+  
+  b kernel_main         // Call kernel_main() function
 
 .section .text
-_vectors:
-	ldr pc, reset_addr      // 0x0
-	ldr pc, undef_addr      // 0x4
-	ldr pc, swi_addr        // 0x8
-	ldr pc, prefetch_addr   // 0xC
-	ldr pc, abort_addr      // 0x10
-	ldr pc, reserved_addr   // 0x14        
-	ldr pc, irq_addr        // 0x18
-	ldr pc, fiq_addr        // 0x1C
-	
+_vectors:                 // vector table
+  ldr pc, reset_addr      // 0x0
+  ldr pc, undef_addr      // 0x4
+  ldr pc, swi_addr        // 0x8
+  ldr pc, prefetch_addr   // 0xC
+  ldr pc, abort_addr      // 0x10
+  ldr pc, reserved_addr   // 0x14        
+  ldr pc, irq_addr        // 0x18
+  ldr pc, fiq_addr        // 0x1C
+  
 reset_addr:     .word reset_handler
 undef_addr:     .word undef_handler
 swi_addr:       .word swi_handler
